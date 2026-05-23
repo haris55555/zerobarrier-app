fa39b7ea8bc0c8af0eed241641773b066456de4d
haris55555 <hai.advisoryservices@gmail.com>
2026-05-24 03:43:28 +0530

    translation fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, onValue, set, remove } from "firebase/database";
 
